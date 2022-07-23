53697ecbc031362f8403f73fd9cc011f0eacc376
jisena.btc <pjohmaths@gmail.com>
2022-07-23 15:00:16 +0300

    add typing simulation

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    console.log(
+        $('.prompt-string.bottom code #prompt').text()
+    );
     let green = true;
+    let str = 'quote';
+    let nextChar = 0;
+    let blinkId, typeId;
 
     function blink () {
         let color =  green ? '#0f0' : '#000';
         green = !green;
 
-        $('.prompt-string.bottom code span').css({
+        $('.prompt-string.bottom code #cursor').css({
             color,
             backgroundColor: color
         });
     }
 
-    setInterval(blink, 500, green);
+    blinkId = setInterval(blink, 500);
+
+    function type () {
+        clearInterval(blinkId);
+        if (nextChar < 5) {
+            $('.prompt-string.bottom code #prompt').text(
+                $('.prompt-string.bottom code #prompt').text() + str[nextChar]
+            );
+
+            nextChar++;
+            return;
+        }
+        // TODO: load new quote here
+        $('.prompt-string.bottom code #prompt').text('[user@wisdom ~]$ '); // restore prompt string
+        clearInterval(typeId);
+    }
+
+    typeId = setInterval(type, 100);
 });
