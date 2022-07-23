6fabcb1c8d579fd993957267f07bfcb2b239f0ec
jisena.btc <pjohmaths@gmail.com>
2022-07-23 12:56:15 +0300

    remove sass - technology overkill

--- util.js
+++
@@ -1,9 +0,0 @@
-const sass = require('sass');
-const fs = require('fs');
-
-const result = sass.compile('styles.scss');
-fs.writeFile('styles.css', result.css, (err) => {
-    if (err) {
-        console.log(err);
-    }
-});
