0dad86063cae5176d4bed92bf61fb6fe2bb46af6
jisena.btc <pjohmaths@gmail.com>
2022-07-23 16:32:08 +0300

    quotes api working

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
+
 $(document).ready(function () {
     let green = true;
-    let str = 'quote';
+    const str = 'quote';
     let nextChar = 0;
+
     let blinkId, 
-        typeId;
+        typeId,
+        quotesData,
+        currentQuote,
+        currentAuthor;
+
+    // set up initial quote
+    $('#tweet-quote').attr(
+          'href',
+          'https://twitter.com/intent/tweet?hashtags=quotes&related=kimjisena&text=' +
+            encodeURIComponent('"If you expect to continue learning all your life, you will be teaching yourself much of the time." ' + '-- ' + 'Richard Hamming')
+    );
+
+    function getQuotes() {
+        return $.ajax({
+            headers: {
+            Accept: 'application/json'
+            },
+            url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+            success: function (data) {
+        if (typeof data === 'string') {
+          quotesData = JSON.parse(data);
+        }
+      }
+    });
+    }
+
+    function getRandomQuote () {
+        return quotesData.quotes[
+          Math.floor(Math.random() * quotesData.quotes.length)
+        ];
+    }
+
+    function getQuote () {
+        let randomQuote = getRandomQuote();
+      
+        currentQuote = randomQuote.quote;
+        currentAuthor = randomQuote.author;
+      
+        $('#tweet-quote').attr(
+          'href',
+          'https://twitter.com/intent/tweet?hashtags=quotes&related=kimjisena&text=' +
+            encodeURIComponent('"' + currentQuote + '" ' + '-- ' + currentAuthor)
+        );
+    }
+
+    function displayQuote () {
+        getQuote();
+        console.log('displaying quote...');
+
+        $("#text code").text(
+            '"'+ currentQuote + '"'
+        );
+        $('#author code').text(
+            '-- ' + currentAuthor
+        );
+    }
 
     function blink () {
         let color =  green ? '#0f0' : '#000';
@@ -14,8 +71,6 @@ $(document).ready(function () {
             backgroundColor: color
         });
     }
-
-    blinkId = setInterval(blink, 500);
 
     function type () {
         clearInterval(blinkId); // stop blinking, we're typing
@@ -35,7 +90,7 @@ $(document).ready(function () {
             return;
         }
         clearInterval(typeId); // done typing
-        // TODO: load new quote here
+        displayQuote(); // display new quote
         $('.prompt-string.bottom code #prompt').text('[user@wisdom ~]$ '); // restore prompt string
         nextChar = 0; // get ready to type again
         blinkId = setInterval(blink, 500); // ok, you can blink now
@@ -45,4 +100,7 @@ $(document).ready(function () {
         typeId = setInterval(type, 150);
     });
 
+    blinkId = setInterval(blink, 500);
+
+    getQuotes();
 });
